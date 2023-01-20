98dc6768bb7387766041ff1523bca6bb593a1e71
Leah Shields <leah.shields@hotmail.co.uk>
2023-01-20 11:42:51 +0000

    Initial commit - basic remix project with empty select parts on home screen

---
+++ app/routes/index.tsx
@@ -0,0 +1,10 @@
+import SelectPart from "./parts/select-part";
+
+
+export default function Index() {
+  return (
+    <main className="relative min-h-screen bg-[#282c34]">
+      <SelectPart />
+    </main>
+  );
+}
---
+++ app/routes/parts/select-part.tsx
@@ -0,0 +1,9 @@
+
+
+export default function SelectPart() {
+  return (
+    <div className="p-[8rem]">
+      <label className="text-white text-3xl">Find a part</label>
+    </div>
+  );
+}
